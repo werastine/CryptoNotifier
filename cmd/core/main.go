3c0fee9d9798140521d4b8df71ebd1069ae254e3
Arsenii Zhadanenko <arseniizh1@gmail.com>
2026-09-16 11:19:26 +0200

    feat: initialize core application structure and server setup

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/werastine/CryptoNotifier/internal/core"
+	"github.com/werastine/CryptoNotifier/pkg/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	cs := core.NewCryptoService()
+	srv := core.NewServer(cs)
+
+	lis, err := net.Listen("tcp", ":50501")
+	if err != nil {
+		log.Printf("[ERROR] listening tcp: %v", err)
+		return
+	}
+
+	defer func() {
+		if err = lis.Close(); err != nil {
+			log.Printf("[ERROR] closing connection: %v", err)
+		}
+	}()
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterCryptoServer(grpcServer, srv)
+
+	if err = grpcServer.Serve(lis); err != nil {
+		log.Printf("[ERROR] serving grpc server: %v", err)
+		return
+	}
+}
